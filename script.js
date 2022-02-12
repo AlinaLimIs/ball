2f64c40b73ced911c715787f26ff892ddaf44b1b
AlinaLimIs <98636942+AlinaLimIs@users.noreply.github.com>
2022-02-12 13:42:15 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// document.getElementById('switch').innerHTML = 'New txt'
+
+
+// let myMove = ()=>{
+//     let id = null;
+//     const elem = document.getElementById('animate')
+//     let pos = 0;
+//      clearInterval(id)
+//      id = setInterval(id)
+//      id = setInterval(frame, 5)
+//      function frame() {
+//          if (pos == 350) {
+//              clearInterval(id)
+//          }else{
+//              pos++;
+//              elem.style.top = pos + 'px'
+//              elem.style.left = pos + 'px'
+//          }
+//      }
+// }
